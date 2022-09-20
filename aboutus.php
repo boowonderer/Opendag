690251595323dd5d4072dd6decb1461cd71091b2
Henkmbo <90178722+Henkmbo@users.noreply.github.com>
2022-09-20 14:12:50 +0200

    about us page

---
+++ aboutus.php
@@ -0,0 +1,35 @@
+<link rel="stylesheet" href="style.css">
+
+
+<div class="content">
+    <strong style="font-size:1.5rem;">MBO Utrecht</strong>
+    <ul>
+        <strong>Bij MBO Utrecht zijn we bereikbaar via telefoon, e-mail en WhatsApp. Heb je een vraag over onze Open
+            Dagen, opleidingen of aanmelden neem contact op via WhatsApp.<strong>
+
+
+    </ul>
+    <br>
+    <strong style="font-size:1.5rem;">Contact</strong>
+    <ul>
+        <li>Vragen over je aanmelding, plaatsing of kennismaking:
+            <ul>WhatsApp: 06-25705051 (op schooldagen bereikbaar tussen 10.00 en 17.00 uur) </ul>
+            Mail: <ul>aanmelden@mboutrecht.nl </ul>
+            Telefoon: <ul> 030 – 281 51 80 (op schooldagen bereikbaar tussen van 10.00 tot 12.00 en van 13.00 tot
+                15.00
+                uur)</ul>
+            Vragen over studiekeuze of opleidingen:
+            <ul> WhatsApp: 06-25705051 (op schooldagen bereikbaar tussen 10.00 en 17.00 uur)
+                <br> Mail: studiekeuze@mboutrecht.nl
+            </ul>
+            Locatie:
+            <ul>Daltonlaan 300
+                3584 BtK Utrecht</ul>
+
+    </ul>
+    </li>
+    </ul>
+
+
+
+</div>
