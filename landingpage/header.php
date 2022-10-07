f66333365297e5c79ade26950c2cd11ddeea0513
boowonderer <333925@student.mboutrecht.nl>
2022-10-07 13:47:08 +0200

    Link werkend gemaakt

--- landingpage/header.php
+++ landingpage/header.php
@@ -1,18 +1,16 @@
+<header>
+  <div class="brand"><a href="#">MBO Utrecht</a></div>
 
-
-  <header>
-        <div class="brand"><a href="#">MBO Utrecht</a></div>
-
-        <nav>
-        <div class="menu-toggle" id="mobile-menu">
+  <nav>
+    <div class="menu-toggle" id="mobile-menu">
       <span class="bar"></span>
       <span class="bar"></span>
       <span class="bar"></span>
     </div>
     <ul class="menu-items">
-                <li><a href="./index.html">Projects</a></li>
-                <li><a href="./enquete.html">Survey</a></li>    
-                <li><a href="./dashboard.php">dashboard</a></li>
-            </ul>
-        </nav>
-    </header>
+      <li><a href="./index.html">Projects</a></li>
+      <li><a href="./enquete.html">Survey</a></li>
+      <li><a href="../dashboard/index.php">dashboard</a></li>
+    </ul>
+  </nav>
+</header>
