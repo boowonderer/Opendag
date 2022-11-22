e08dc277eb381a013b5aabe1908dd84a9032b2c8
korné <k.rijninks@gmail.com>
2022-11-22 13:36:29 +0100

    change: FAQ PAGE begin met vragen

---
+++ FAQpage/index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="style.css">
+
+
+
+    <!-- wordt alleen gebruikt voor incons in footer -->
+
+    <title>Opendag | MBO | FAQ</title>
+</head>
+
+<body>
+
+    <div id="FAQ">
+            <div class="col">
+                <h2>FAQ</h2>
+                <div class="vragen">
+                    <div class="vraag">
+                        <input type="radio" id="vraag1" name="rd">
+                        <label class="tab-label" for="vraag1">Wat zijn de verschillende niveaus op het mbo?</label>
+                        <div class="awnser">
+                            <p class="awnsers">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, facilis.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="vraag">
+                        <input type="radio" id="vraag2" name="rd">
+                        <label class="tab-label" for="vraag2">Wat is het verschil tussen een BOL en een BBL opleiding?</label>
+                        <div class="awnser">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil, aut.
+                        </div>
+                    </div>
+                    <div class="vraag">
+                        <input type="radio" id="vraag3" name="rd">
+                        <label class="tab-label" for="vraag3">Wat kost een mbo-opleiding?</label>
+                        <div class="awnser">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil, aut.
+                        </div>
+                    </div>
+                    <div class="vraag">
+                        <input type="radio" id="vraag4" name="rd">
+                        <label class="tab-label" for="vraag4">Heb ik een laptop nodig?</label>
+                        <div class="awnser">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil, aut.
+                        </div>
+                    </div>
+                    
+            </div>
+        </div>
+    </div>
+
+
+
+
+</body>
+
+</html>
