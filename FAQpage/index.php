3fcf6c01a057ddb1f950740903dfcf69b141ecfc
GerwindeHeus <90325732+GerwindeHeus@users.noreply.github.com>
2022-12-20 11:35:28 +0100

    header veranderd

--- FAQpage/index.php
+++ FAQpage/index.php
@@ -17,123 +17,129 @@
 
 
     <div class="banner">
-    
-    <div id="FAQ">
-        <div class="sidenav">
-            <a href="overMbo.php">Over mbo</a>
-            <a href="Studiekeuze.php">Over Studiekeuze</a>
-            <a href="aanmelden.php">Over aanmelden en plaatsen</a>
-            <a href="Schoolkosten.php">Over Schoolkosten</a>
-        </div>
+
+        <div id="FAQ">
+            <div class="sidenav">
+                <a href="overMbo.php">Over mbo</a>
+                <a href="Studiekeuze.php">Over Studiekeuze</a>
+                <a href="aanmelden.php">Over aanmelden en plaatsen</a>
+                <a href="Schoolkosten.php">Over Schoolkosten</a>
+            </div>
 
 
-        <div class="col">
-            <div class="vragen">
+            <div class="col">
+                <div class="vragen">
 
-            <h1> Over mbo </h1>
-            <p> Op deze pagina vind je antwoorden op onze meest gestelde vragen over het mbo. De verschillende niveaus, toelatingseisen en de verschillende soorten opleidingen (BOL/BBL) komen aan bod. Je kunt hier ook terecht voor informatie over MHBO-opleidingen en over de opbouw van een opleiding bij MBO Utrecht.</p>
-                <div class="vraag">
-                    <input type="checkbox" id="vraag1" name="cb">
-                    <label class="tab-label" for="vraag1">Wat zijn de verschillende niveaus op het mbo?</label>
-                    <div class="awnser">
-                        <p>
-                            Het mbo heeft opleidingen op 4 niveaus. Voor elk niveau gelden andere toelatingseisen. Ook
-                            het aantal jaar dat je naar school gaat verschilt.</p>
-                        <ul>
-                            <li> <strong>Niveau 1</strong>
-                                <br>
-                                Vooropleiding: Geen diploma vereist, minimaal 16 jaar op 1 augustus
-                                <br>
-                                Duur: 1 jaar<br>
-                                Doorstromen: mbo niveau 2<br>
-                                Bekijk hier onze entree-opleiding
-                            </li>
-                            <li>
-                                <strong>Niveau 2</strong>
-                                <br>
-                                Vooropleiding: Diploma vmbo basis/kader/GL/TL(MAVO) of een diploma mbo niveau 1
-                                <br>
-                                Duur: 1-2 jaar
-                                <br>
-                                Doorstromen: mbo niveau 3 of 4
-                                <br>
-                                Bekijk hier onze niveau 2 opleidingen
-                            </li>
-                            <li> <strong>Niveau 3</strong><br>
-                                Vooropleiding: Diploma vmbo kader/GL/TL(MAVO), diploma mbo niveau 2 of een
-                                overgangsbewijs
-                                Havo / VWO van leerjaar 3 naar 4<br>
-                                Duur: 2-3 jaar<br>
-                                Doorstromen: mbo niveau 4<br>
-                                Bekijk hier onze niveau 3 opleidingen<br> </li>
-                            <li> <strong>Niveau 4</strong><br>
-                                Vooropleiding: Diploma vmbo kader/GL/TL(MAVO), diploma mbo niveau 2 en 3 of een<br>
-                                overgangsbewijs Havo / VWO van leerjaar 3 naar 4<br>
-                                Duur: 3-4 jaar<br>
-                                Doorstromen: hbo<br>
-                                Bekijk hier onze niveau 4 en MHBO opleidingen<br></li>
-                        </ul>
+                    <h1> Over mbo </h1>
+                    <p> Op deze pagina vind je antwoorden op onze meest gestelde vragen over het mbo. De verschillende
+                        niveaus, toelatingseisen en de verschillende soorten opleidingen (BOL/BBL) komen aan bod. Je
+                        kunt hier ook terecht voor informatie over MHBO-opleidingen en over de opbouw van een opleiding
+                        bij MBO Utrecht.</p>
+                    <div class="vraag">
+                        <input type="checkbox" id="vraag1" name="cb">
+                        <label class="tab-label" for="vraag1">Wat zijn de verschillende niveaus op het mbo?</label>
+                        <div class="awnser">
+                            <p>
+                                Het mbo heeft opleidingen op 4 niveaus. Voor elk niveau gelden andere toelatingseisen.
+                                Ook
+                                het aantal jaar dat je naar school gaat verschilt.</p>
+                            <ul>
+                                <li> <strong>Niveau 1</strong>
+                                    <br>
+                                    Vooropleiding: Geen diploma vereist, minimaal 16 jaar op 1 augustus
+                                    <br>
+                                    Duur: 1 jaar<br>
+                                    Doorstromen: mbo niveau 2<br>
+                                    Bekijk hier onze entree-opleiding
+                                </li>
+                                <li>
+                                    <strong>Niveau 2</strong>
+                                    <br>
+                                    Vooropleiding: Diploma vmbo basis/kader/GL/TL(MAVO) of een diploma mbo niveau 1
+                                    <br>
+                                    Duur: 1-2 jaar
+                                    <br>
+                                    Doorstromen: mbo niveau 3 of 4
+                                    <br>
+                                    Bekijk hier onze niveau 2 opleidingen
+                                </li>
+                                <li> <strong>Niveau 3</strong><br>
+                                    Vooropleiding: Diploma vmbo kader/GL/TL(MAVO), diploma mbo niveau 2 of een
+                                    overgangsbewijs
+                                    Havo / VWO van leerjaar 3 naar 4<br>
+                                    Duur: 2-3 jaar<br>
+                                    Doorstromen: mbo niveau 4<br>
+                                    Bekijk hier onze niveau 3 opleidingen<br> </li>
+                                <li> <strong>Niveau 4</strong><br>
+                                    Vooropleiding: Diploma vmbo kader/GL/TL(MAVO), diploma mbo niveau 2 en 3 of een<br>
+                                    overgangsbewijs Havo / VWO van leerjaar 3 naar 4<br>
+                                    Duur: 3-4 jaar<br>
+                                    Doorstromen: hbo<br>
+                                    Bekijk hier onze niveau 4 en MHBO opleidingen<br></li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-                <div class="vraag">
-                    <a href="#vraag2"></a> <input type="checkbox" id="vraag2" name="cb">
-                    <label class="tab-label" for="vraag2">Wat is het verschil tussen een BOL en een BBL
-                        opleiding?</label>
-                    <div class="awnser">
-                        <p>
-                            Het mbo heeft twee verschillende leerwegen: BOL en BBL. Zij verschillen van elkaar in de
-                            verdeling tussen theorie en praktijk.
-                        <p>
-                        <ul>
-                            <strong>BOL</strong><br>
-                            <li class="nostyleli">
-                                BOL staat voor beroepsopleidende leerweg. Bij een BOL-opleiding ga je de hele week naar
-                                school
-                                en doe je praktijkervaring op tijdens de stage bij een erkend leerbedrijf.
-                            </li>
-                            <strong>BBL</strong><br>
-                            <li class="nostyleli">
-                                BBL staat voor beroepsbegeleidende leerweg. Bij een BBL-opleiding ga je één dag per week
-                                naar
-                                school en werk je een minimaal aantal vastgestelde uren per week bij een erkend
-                                leerbedrijf.
-                            </li>
-                        </ul>
+                    <div class="vraag">
+                        <a href="#vraag2"></a> <input type="checkbox" id="vraag2" name="cb">
+                        <label class="tab-label" for="vraag2">Wat is het verschil tussen een BOL en een BBL
+                            opleiding?</label>
+                        <div class="awnser">
+                            <p>
+                                Het mbo heeft twee verschillende leerwegen: BOL en BBL. Zij verschillen van elkaar in de
+                                verdeling tussen theorie en praktijk.
+                            <p>
+                            <ul>
+                                <strong>BOL</strong><br>
+                                <li class="nostyleli">
+                                    BOL staat voor beroepsopleidende leerweg. Bij een BOL-opleiding ga je de hele week
+                                    naar
+                                    school
+                                    en doe je praktijkervaring op tijdens de stage bij een erkend leerbedrijf.
+                                </li>
+                                <strong>BBL</strong><br>
+                                <li class="nostyleli">
+                                    BBL staat voor beroepsbegeleidende leerweg. Bij een BBL-opleiding ga je één dag per
+                                    week
+                                    naar
+                                    school en werk je een minimaal aantal vastgestelde uren per week bij een erkend
+                                    leerbedrijf.
+                                </li>
+                            </ul>
 
+                        </div>
                     </div>
-                </div>
-                <div class="vraag">
-                    <input type="checkbox" id="vraag3" name="cb">
-                    <label class="tab-label" for="vraag3">Wat kost een mbo-opleiding?</label>
-                    <div class="awnser">
-                        <p>De kosten van een mbo-opleiding zijn afhankelijk van de leerweg (BOL/BBL) en je leeftijd.
-                            Deze
-                            kosten zijn wettelijk vastgesteld en vind je op de website van Rijksoverheid.</p>
-                        <ul>
-                            <li class="nostyleli">
-                                Het lesgeld voor schooljaar 2022-2023 kun je vinden op de website van DUO.
-                            </li>
-                            <li class="nostyleli">
-                                Naast dit lesgeld komen er nog kosten bij voor de boeken en andere spullen die je nodig
-                                hebt
-                                voor je opleiding.
-                            </li>
-                        </ul>
+                    <div class="vraag">
+                        <input type="checkbox" id="vraag3" name="cb">
+                        <label class="tab-label" for="vraag3">Wat kost een mbo-opleiding?</label>
+                        <div class="awnser">
+                            <p>De kosten van een mbo-opleiding zijn afhankelijk van de leerweg (BOL/BBL) en je leeftijd.
+                                Deze
+                                kosten zijn wettelijk vastgesteld en vind je op de website van Rijksoverheid.</p>
+                            <ul>
+                                <li class="nostyleli">
+                                    Het lesgeld voor schooljaar 2022-2023 kun je vinden op de website van DUO.
+                                </li>
+                                <li class="nostyleli">
+                                    Naast dit lesgeld komen er nog kosten bij voor de boeken en andere spullen die je
+                                    nodig
+                                    hebt
+                                    voor je opleiding.
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-                <div class="vraag">
-                    <input type="checkbox" id="vraag4" name="cb">
-                    <label class="tab-label" for="vraag4">Heb ik een laptop nodig?</label>
-                    <div class="awnser">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil, aut.
+                    <div class="vraag">
+                        <input type="checkbox" id="vraag4" name="cb">
+                        <label class="tab-label" for="vraag4">Heb ik een laptop nodig?</label>
+                        <div class="awnser">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil, aut.
+                        </div>
                     </div>
-                </div>
 
+                </div>
             </div>
         </div>
     </div>
-    </div>
-
 
 
 </body>
--- landingpage/header.php
+++ landingpage/header.php
@@ -10,7 +10,7 @@
         <ul class="menu-items">
             <li><a href="../differentCourses/index.php">Opleidingen</a></li>
             <li><a href="../carousel/enquete.php">Survey</a></li>
-            <li><a href="../dashboard/index.php">Dashboard</a></li>
+            <li><a href="../FAQpage/index.php">FAQ</a></li>
             <li><a href="../aboutus/index.php">About us</a></li>
             <li><a href="../notifications/index.php">Notifications</a></li>
         </ul>
